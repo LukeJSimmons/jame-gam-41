e58716502031dd20220b83915f279cb669e4c87c
LukeJSimmons <lucasjsimmons@yahoo.com>
2025-04-04 11:24:13 -0500

    Add movement between doors

--- scenes/door/door.gd
+++ scenes/door/door.gd
@@ -1,13 +1,26 @@
 extends Area3D
 
 @onready var mesh = $MeshInstance3D
+@onready var exit_position = $exit_position
 @onready var room = get_parent()
 
-var color: Color
+@export var color: Color
+@export var exit_door_path: NodePath
+
+var exit_door = null
 
 
 func _ready() -> void:
-	randomize()
+	exit_door = get_node(exit_door_path)
 	var new_color = mesh.get_surface_override_material(0)
-	new_color.albedo_color = Color(randf_range(0,1), randf_range(0,1), randf_range(0,1))
-	color = new_color.albedo_color
+	if !color:
+		create_color()
+	new_color.albedo_color = color
+
+func create_color():
+	randomize()
+	color = Color(randf_range(0,1), randf_range(0,1), randf_range(0,1))
+
+func enter(player):
+	player.position = exit_door.exit_position.global_position
+	player.rotation.y = exit_door.exit_position.rotation.y
